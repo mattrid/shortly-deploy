fb7b4b74c16b1a5c6b6a70396ba34f75ba2256da
MKS-Student <mks.student@hackreactor.com>
2017-03-01 18:47:31 -0800

    change port number

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var app = require('./server-config.js');
 
-var port = 4569;
+var port = 4568;
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+console.log('does this work')
